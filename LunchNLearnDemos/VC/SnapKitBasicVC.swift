da2139300087e98c005c444c860c6141a6490125
Brendan Lensink <blensink192@gmail.com>
2018-03-09 09:45:55 -0800

    add skeleton for the demo project with snapkit examples

---
+++ LunchNLearnDemos/VC/SnapKitBasicVC.swift
@@ -0,0 +1,9 @@
+//
+//  SnapKitBasicVC.swift
+//  LunchNLearnDemos
+//
+//  Created by Brendan Lensink on 2018-03-09.
+//  Copyright © 2018 Steamclock. All rights reserved.
+//
+
+import Foundation
---
+++ LunchNLearnDemos/VC/SnapKitLoginVC.swift
@@ -0,0 +1,9 @@
+//
+//  SnapKitLoginVC.swift
+//  LunchNLearnDemos
+//
+//  Created by Brendan Lensink on 2018-03-09.
+//  Copyright © 2018 Steamclock. All rights reserved.
+//
+
+import Foundation
